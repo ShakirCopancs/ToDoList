0f487816c0fab1a71deff0f3995661f701e2aa9c
ShakirAhmad <shakir.ahmad@copancs.com>
2023-07-23 22:32:45 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React, { useState } from "react";
-import './App.css';
+import "./App.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPencil } from "@fortawesome/free-solid-svg-icons";
-
+import { faRemove } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   const [todo, setTodo] = useState("");
   const [list, setList] = useState([]);
 
-  function handleTodo (e) {
+  function handleTodo(e) {
     setTodo(e.target.value);
   }
 
@@ -29,35 +28,17 @@ function App() {
       handleAdd();
     }
   }
-  
 
   return (
     <div className="App">
-      <div className='main'>
-        <section id="todoList" >
-          <header>
-            <h1>
-              Todo App
-            </h1>
+      <div className="main">
+        <section id="todoList">
+          <header className="site-header">
+            <h1>Todo App</h1>
             <p>Completed: 0/{list.length} </p>
           </header>
 
-          <button onClick={handleAdd}>
-            <span>
-              <svg
-                stroke="currentColor"
-                fill="currentColor"
-                strokeWidth="0"
-                viewBox="0 0 512 512"
-                height="1em"
-                width="1em"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path d="M416 277.333H277.333V416h-42.666V277.333H96v-42.666h138.667V96h42.666v138.667H416v42.666z"></path>
-              </svg>
-            </span>
-          </button>
-          <div className='input'>
+          <div className="input">
             <input
               type="text"
               placeholder="add a todo..."
@@ -66,36 +47,39 @@ function App() {
               onChange={(e) => handleTodo(e)}
               onKeyDown={handleKeyPress}
             />
+            <button onClick={handleAdd} className="plusBtn">
+              <span className="addPlus">
+                <svg
+                  stroke="currentColor"
+                  fill="currentColor"
+                  strokeWidth="0"
+                  viewBox="0 0 512 512"
+                  height="1em"
+                  width="1em"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M416 277.333H277.333V416h-42.666V277.333H96v-42.666h138.667V96h42.666v138.667H416v42.666z"></path>
+                </svg>
+              </span>
+            </button>
           </div>
 
-          <ul>
+          <ul className="listItems">
             {list.map((item, index) => (
-              <li key={index}> 
-                <span className='completed'>
+              <li key={index}>
+                <span className="completed">
                   <input id={`checkbox-${index}`} type="checkbox" />
                   {item}
                 </span>
-           
-                
-                  <FontAwesomeIcon icon={faPencil}  />
-             
-                <button className="danger" onClick={()=>handleDelete(index)}>
-                  <svg
-                    stroke="currentColor"
-                    fill="none"
-                    strokeWidth="2"
-                    viewBox="0 0 24 24"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    height="1em"
-                    width="1em"
-                    xmlns="http://www.w3.org/2000/svg"
+
+                <span className="todoicons">
+                  <button
+                    className="danger"
+                    onClick={() => handleDelete(index)}
                   >
-                    <path d="M21 4H8l-7 8 7 8h13a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2z"></path>
-                    <line x1="18" y1="9" x2="12" y2="15"></line>
-                    <line x1="12" y1="9" x2="18" y2="15"></line>
-                  </svg>
-                </button>
+                    <FontAwesomeIcon icon={faRemove} />
+                  </button>
+                </span>
               </li>
             ))}
           </ul>
